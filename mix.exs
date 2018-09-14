2c171f78f05fb334a6904e5bb4047a1f9e2a262b
Mirko van der Waal <mirko.van.der.waal@holder.nl>
2018-09-14 09:34:16 +0200

    Add mix files.

---
+++ mix.exs
@@ -0,0 +1,22 @@
+defmodule INI.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ini,
+      version: "0.0.1",
+      elixir: "~> 1.6",
+      test_coverage: [tool: ExCoveralls],
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [extra_applications: [:logger]]
+  end
+
+  defp deps do
+    [{:excoveralls, "~> 0.10", only: :test}]
+  end
+end
